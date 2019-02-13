1f5c2eb37a3cd44e60cf393c10269e96c34f3d46
Jason.Yu <morty.yu@yahoo.com>
2019-02-13 15:57:03 +0800

    Model and kpath

--- src/JLTB.jl
+++ src/JLTB.jl
@@ -1,7 +1,27 @@
 module JLTB
 
-export Hamiltonian, eigenspectrum
+mutable struct Model
+    lattice::Matrix{Float64}
+    orbitals::Matrix{Float64}
+    onsites::Vector{Float64}
+    hoppings::Vector{Hopping}
+    function Model(lattice::T, orbitals::T, onsites::Vector{Float64}) where T<:Matrix{Float64}
 
-include("hamiltonian.jl")
+    end
+end
+
+function kpath(rlatt::Matrix{Float64}, path::Matrix{Float64}, nk::Int64=300)
+    nnodes = size(path, 1)
+    dim = size(path, 2)
+    kvectors = zeros(nk, dim)
+    kdists = zeros(nk)
+    knodes = zeros(nnodes)
+    
+    return kvectors, kdists, knodes
+end
+
+function eigenspectrum(m::Model, kvectors)
+    return evals
+end
 
 end
