310661d37ae30b64f2aee4f58527ee87d654345e
Jason.Yu <morty.yu@yahoo.com>
2019-03-05 19:32:39 +0800

    add graph done

--- src/graph.jl
+++ src/graph.jl
@@ -1,18 +1,60 @@
 module graph
 
-struct Site{T<:Vector{Float64}}
-    dim::Int64
-    position::T
-    periodic::T
-    function Site{T}(position::T, periodic::T) where T<:Vector{Float64}
-        dim = length(position)
-        @assert dim == length(periodic)
-        new(dim, position, periodic)
-    end
+# struct Site{T<:Vector{Float64}}
+#     dim::Int64
+#     position::T
+#     periodic::T
+#     cartesian::Bool
+#     function Site{T}(position::T, periodic::T, cartesian::Bool) where T<:Vector{Float64}
+#         dim = length(position)
+#         @assert dim == length(periodic)
+#         new(dim, position, periodic, cartesian)
+#     end
+# end
+#
+# Site(position::T, periodic::T, cartesian::Bool) where T<:Vector{Float64} = Site{T}(position, periodic, cartesian)
+# Site(position, periodic, cartesian::Bool) = Site(convert(Vector{Float64}, position), convert(Vector{Float64}, periodic), cartesian)
+# Site(position::T, periodic::T) where T<:Vector{Float64} = Site{T}(position, periodic, false)
+# Site(position, periodic) = Site(convert(Vector{Float64}, position), convert(Vector{Float64}, periodic), false)
+#
+# function toCartesian(s::Site, lattice::Matrix{Float64})
+#     position = s.position + s.periodic
+#     carPos = lattice * position
+#     return Site(carPos, s.periodic, true)
+# end
+#
+# struct LinkGraph
+#     link::Dict{Site, Vector{Site}}
+#     function LinkGraph(l::T) where T<:Dict{Site, Vector{Site}}
+#         new(l)
+#     end
+# end
+
+mutable struct Graph
+    lattice::Matrix{Float64}
+    orbTable::Dict{Int64, Vector{Float64}}
+    links::Dict{Site, Vector{Site}}
 end
 
-Site(position::T, periodic::T) where T<:Vector{Float64} = Site{T}(position, periodic)
-Site(position, periodic) = Site(convert(Vector{Float64}, position), convert(Vector{Float64}, periodic))
-Site(position::Tuple{Vararg{T}}, periodic::Tuple{Vararg{T}}) where T<:Real = Site(collect(position), collect(periodic))
+struct Site
+    idx::Int64
+    periodic::Vector{Int64}
+end
+
+# Graph(lattice::Matrix{Float64}, lg::LinkGraph) = Graph(lattice, lg.link)
+
+function Hop(g::Graph, fn::Function)
+    Hoppings = Vector{Hopping}(undef, 0)
+    for k, vs in g.link
+        for s in vs
+            indI = k.idx
+            indJ = s.idx
+            amp = fn(g, k, s)
+            periodic = s.periodic
+            push!(Hoppings, Hopping(amp, indI, indJ, periodic))
+        end
+    end
+    return Hoppings
+end
 
 end # module
--- test/test_graph.jl
+++ test/test_graph.jl
@@ -1,29 +1,30 @@
 using Test
 using JLTB
+using LinearAlgebra
 
 @testset "graph" begin
 
+# test Site struct
+# s0 = Site([1//2, 1//3], [-2, 0], false)
+# s0Car = toCartesian(s0, [3.0 0.0; 6.0 12.0])
+# @test s0Car.cartesian == true
+# @test 1e-12 > norm(s0Car.position - [-4.5, -5.0])
+
 # test LinkGraph struct
-linkInfo = Dict{Site, Vector{Site}}()
-s1 = Site((2//3, 1//3), (0, 0))
-s2 = Site((0, 0), (0, 0))
-s3 = Site((0, 0.0), (1, 1))
-s4 = Site((0.0, 0.0), (1, 0))
-linkInfo[s1] = [s2, s3, s4]
-lg = LinkGraph(linkInfo)
-
-
 latt = [3.0 0.0; -1.5 1.5*√3]
-g = Graph(latt, lg)
-
-function fn(s0, s1)
-    if distance(s0, s1) > 2
+s1 = Site(1, [0, 0])
+s2 = Site(2, [0, 0])
+s3 = Site(1, [1, 0])
+s4 = Site(1, [1, 1])
+orbTable = Dict{Int64, Vector{Float64}}(1 => [0.0, 0.0], 2=> [2/3, 1/3])
+function fn(g, sx, sy)
+    if distance(sx_car, sy_car) > 2
         return 0.6
     end
     return 0.0
 end
 
-hops = getHop(g, fn)
+hops = Hop(g, fn)
 @test size(hops) == (3, 1)
 for h in hops
     @test h.amp == 0.6
